800a533d2762895c5f9645a81abfd1372c68f4a8
sRowbertt <lorenzoestacuyr43@gmail.com>
2026-07-23 08:57:15 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+<<<<<<< HEAD
+//=====================================
+// CARGABOT
+//=====================================
+
+// Cambiar fondo del menú al hacer scroll
+
+window.addEventListener("scroll", function () {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+=======
+//=====================================
+// CARGABOT
+//=====================================
+
+// Cambiar fondo del menú al hacer scroll
+
+window.addEventListener("scroll", function () {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+>>>>>>> 2a82998 (Agregado favicon y corregido logo)
+});
